bccd6430a38c8e4e71a0ce557d7335cf8d55df5f
tomasgallego <tomas.gallego08@hotmail.com>
2026-03-13 19:51:28 -0300

    un poquito

--- src/utils/api_call.js
+++ src/utils/api_call.js
@@ -1,9 +1,31 @@
-export const call_data = () => {
-     fetch(`https://api.rawg.io/api/games?key=${apiKey}`)
-        .then(res => res.json())
-        .then(data => {
-            const games = data.results;
-        });
+const apiKey = '417390b221e44959848516f93177b905';
 
-    return games;
+export const call_data = async () => {
+    const response = await fetch(`https://api.rawg.io/api/games?key=${apiKey}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const call_data_by_id = async (id) => {
+    const response = await fetch(`https://api.rawg.io/api/games/${id}?key=${apiKey}`);
+    const data = await response.json();
+    return data;
+};
+
+export const call_data_by_search = async (search) => {
+    const response = await fetch(`https://api.rawg.io/api/games?search=${search}&key=${apiKey}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const call_data_by_genre = async (genre) => {
+    const response = await fetch(`https://api.rawg.io/api/games?genres=${genre}&key=${apiKey}`);
+    const data = await response.json();
+    return data.results;
+}
+
+export const top_games = async () => {
+    const response = await fetch(`https://api.rawg.io/api/games?ordering=-metacritic&key=${apiKey}`);
+    const data = await response.json();
+    return data.results;
 }
